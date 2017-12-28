c2a69f78eef1849d8ee453c3d69466d8655438a5
Mainul Hasan <Mainul Hasan>
2017-12-28 18:30:04 +0600

    SpringMVC_Security_JavaConfig-in-Memory-authentication

---
+++ SpringMVC_Security_JavaConfig-in-Memory-authentication/src/main/java/com/mainul35/model/User.java
@@ -0,0 +1,82 @@
+package com.mainul35.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    Long id;
+    String name;
+    String email;
+    String phoneNumber;
+    Integer age;
+    String sex;
+    String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
